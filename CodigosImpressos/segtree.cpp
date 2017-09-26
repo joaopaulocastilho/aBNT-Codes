041839350cbb37e7de7b3a34c46f83b47f7c6857
João Paulo Kuboszewski Castilho <joao.pkc@gmail.com>
2017-09-25 23:19:32 -0300

    Seg tree atualizada. Lamento, mas o código está nos meus padrões

--- CodigosImpressos/segtree.cpp
+++ CodigosImpressos/segtree.cpp
@@ -1,69 +1,29 @@
-<<<<<<< HEAD
-#include <cstdio>
-#include <cstring>
-=======
 #include <stdio.h>
 #include <string.h>
->>>>>>> Castilho
 
 #define MAX 11234
 #define left(p) (p) << 1
 #define right(p) ((p) << 1) + 1
 
-<<<<<<< HEAD
-
-int a[MAX], n, st[4*MAX];
-
-void build(int p, int l, int r){
-  int p1, p2;
-
-  if(l == r){ st[p] = l; return; }
-=======
 int a[MAX], n, st[4*MAX];
 
 void build(int p, int l, int r) {
   int p1, p2;
   if(l == r) { st[p] = l; return; }
->>>>>>> Castilho
   build(left(p), l, (l+r)/2);
   build(right(p), (l+r)/2 + 1, r);
   p1 = st[left(p)]; p2 = st[right(p)];
   st[p] = a[p1] <= a[p2] ? p1 : p2;
 }
 
-<<<<<<< HEAD
-int rmq(int p, int l, int r, int i, int j){
-  int p1, p2;
-  
-=======
 int rmq(int p, int l, int r, int i, int j) {
   int p1, p2;
->>>>>>> Castilho
   if(i > r || j < l) return -1; //Esse segmento não está no intervalo necessario
   if(l >= i && r <= j) return st[p]; //Esse segmento esta inteiro na query
   p1 = rmq(left(p), l, (l+r)/2, i, j);
   p2 = rmq(right(p), (l+r)/2 + 1, r, i, j);
   if(p1 == -1) return p2;
   if(p2 == -1) return p1;
-<<<<<<< HEAD
-  return (a[p1] <= a[p2]) ? p1 : p2;  
-}
-
-void update(int p, int l, int r, int i){
-  if(i > r || i < l) return; //Fora do escopo.
-  if(l >= i && r <= i){ //Eh a folha que eu quero.
-    for(i = p >> 1; i >= 1; i = i >> 1){ // Sobe ate a raiz atualizando rmq.
-      int p1 = st[left(i)], p2 = st[right(i)];
-      st[i] = a[p1] <= a[p2] ? p1 : p2;
-    }
-    return;
-  }
-  update(left(p), l, (l+r)/2, i);
-  update(right(p), (l+r)/2 + 1, r, i);
-}
-
-
-=======
   return (a[p1] <= a[p2]) ? p1 : p2;
 }
 
@@ -77,18 +37,11 @@ void update(int p, int l, int r, int i) {
   st[p] = a[p1] <= a[p2] ? p1 : p2;
 }
 
->>>>>>> Castilho
 int main(void){
   int i, j, b ,c;
   scanf("%d", &n);
   memset(st, -1, sizeof(st));
-<<<<<<< HEAD
-  for(i = 0; i < n; i++){
-    scanf("%d", &a[i]);
-  }
-=======
   for(i = 0; i < n; i++) scanf("%d", &a[i]);
->>>>>>> Castilho
   build(1, 0, n-1);
   for(i = 1; st[i] != -1; i++) printf("%d ", st[i]); // printa
   scanf("%d %d", &i, &j);
